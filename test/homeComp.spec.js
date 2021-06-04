07aa845a086a83b56c970b000a089814eeccd0d0
Daniella Aceves <daniella.aceves98@gmail.com>
2021-06-04 11:09:34 -0400

    react tests attempted to write: DOM error

---
+++ test/homeComp.spec.js
@@ -0,0 +1,26 @@
+// import {expect} from 'chai'
+// import React from 'react'
+// import enzyme, {mount} from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16'
+// import { HomePage } from '../client/components/HomePage'
+// import { mockAxios } from './setup';
+// import { App } from '../client/App'
+// import history from '../client/history'
+
+// const getRequests = () => mockAxios.history.get;
+
+// // const adapter = new Adapter()
+// // enzyme.configure({adapter})
+
+// describe('Home', () => {
+//   let home
+
+//   // beforeEach(() => {
+//   //   home = shallow(<HomePage username="cody" />)
+//   // })
+
+//   // it('renders the email in an h3', () => {
+//   //  const wrapper = mount(<HomePage username = "Dani" />)
+//   //  expect(wrapper).to.include.text('Dani')
+//   // })
+// })
